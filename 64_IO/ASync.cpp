bbc2a07677f0bc5c9d387d3b8e767371a393ae39
yura <yura0525@naver.com>
2018-10-18 17:55:12 +0900

    iocp

--- 64_IO/ASync.cpp
+++ 64_IO/ASync.cpp
@@ -1,8 +1,13 @@
-
+/*
 #include <windows.h>
 #include <iostream>
 #include <tchar.h>
 #include <string.h>
+
+OVERLAPPED ov;
+LARGE_INTEGER	g_size;
+char* g_buf = NULL;
+
 //유니코드에서 멀티바이트로 바꾸는 함수.
 char* GetW2M(WCHAR* data)
 {
@@ -39,68 +44,95 @@ std::wstring m2w(std::string data)
 	return ret;
 }
 
+//4096 * 1024만큼 쪼개서 읽는다.
+//파일을 한번에 읽으면 부담스럽기 때문에 쪼개서 읽는다.
+void FileRead(HANDLE hReadFile, DWORD offset)
+{
+	DWORD dwLength = 4096 * 1024;			//dwLength = filesize.QuadPart;
+	g_size.QuadPart += offset;
+
+	ov.Offset = g_size.LowPart;
+	ov.OffsetHigh = g_size.HighPart;
+	char* pTemp = &g_buf[g_size.QuadPart];
+
+	DWORD dwRead;
+	bool iRet = ReadFile(hReadFile, 
+						pTemp,
+						dwLength,
+						&dwRead, 
+						&ov);
+
+	//비동기입출력함수. OVERLAPPED을 사용했다.
+	//그래서 읽기가 끝나기 전에 리턴되어서 iRet == FALSE이므로
+	//GetLastError() == ERROR_IO_PENDING으로 읽고있는 중인지 확인해야한다.
+	//읽기가 다 끝났을때 통지를 해줘야한다.
+	if (iRet == FALSE)
+	{
+		if (GetLastError() == ERROR_IO_PENDING)
+		{
+			printf("\n%s", "ERROR_IO_PENDING Read...");
+		}	
+	}
+}
 void main()
 {
-	char* buf = NULL;
 	LARGE_INTEGER filesize;
-	DWORD dwLength;
 
-	OVERLAPPED ov;
 	ZeroMemory(&ov, sizeof(OVERLAPPED));
 	//wov.hEvent = CreateEvent();
 
-
-	HANDLE hReadFile = CreateFileA("../../data/Dance The Night Away.flac", GENERIC_READ, 0, NULL,
-		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, NULL);
+	HANDLE hReadFile = CreateFileA(
+									"../../data/data_1.zip", 
+									GENERIC_READ, 
+									0, 
+									NULL,
+									OPEN_EXISTING, 
+									FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, 
+									NULL);
 	if (hReadFile != INVALID_HANDLE_VALUE)
 	{
 		GetFileSizeEx(hReadFile, &filesize);
 
-		buf = new char[filesize.QuadPart];
-		dwLength = filesize.QuadPart;
-		DWORD dwRead;
-
-		bool iRet = ReadFile(hReadFile, buf, dwLength, 
-			&dwRead, &ov);
-
-		//비동기입출력함수. OVERLAPPED을 사용했다.
-		//그래서 읽기가 끝나기 전에 리턴되어서 iRet == FALSE이므로
-		//GetLastError() == ERROR_IO_PENDING으로 읽고있는 중인지 확인해야한다.
-		//읽기가 다 끝났을때 통지를 해줘야한다.
-		if (iRet == FALSE)
+		g_buf = new char[filesize.QuadPart];
+		if (g_buf == NULL)
 		{
-			if (GetLastError() == ERROR_IO_PENDING)
-			{
-				printf("\n%s", "ERROR_IO_PENDING Read...");
-			}
+			return;
 		}
-		
-		while (1)
+		FileRead(hReadFile, 0);
+	}
+
+	while (1)
+	{
+		DWORD trans = 0;
+		bool ret = GetOverlappedResult(hReadFile, &ov, &trans, TRUE);
+		if (ret == TRUE)
 		{
-			DWORD trans;
-			bool ret = GetOverlappedResult(hReadFile, &ov, &trans, FALSE);
-			if (ret == TRUE)
+			if (filesize.QuadPart <= g_size.QuadPart)
 			{
-				printf("\n%s", "finishRead...");
 				break;
 			}
-			else
-			{
-				printf("\n%s", "Read...");
-			}
+			FileRead(hReadFile, trans);
+			printf("\n%s : %ld", "FinishRead...", g_size.QuadPart);
 		}
-	}
+		else
+		{
+			printf("\n%s", "READ...");
+		}
+	}	
 	CloseHandle(hReadFile);
 
-	
-	HANDLE hWriteFile = CreateFileA("copy.flac", 
-		GENERIC_READ | GENERIC_WRITE, 0, NULL,
-		CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, NULL);
+	OVERLAPPED wov;
+	ZeroMemory(&wov, sizeof(OVERLAPPED));
+	HANDLE hWriteFile = CreateFileA("data_1.zip", 
+									GENERIC_READ | GENERIC_WRITE, 0, NULL,
+									CREATE_ALWAYS,
+									FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, 
+									NULL);
 	if (hWriteFile != NULL)
 	{
 		DWORD dwWritten;
-		bool iRet = WriteFile(hWriteFile, buf, dwLength, 
-								&dwWritten, &ov);
+		bool iRet = WriteFile(hWriteFile, g_buf, filesize.QuadPart, 
+								&dwWritten, &wov);
 
 		if (iRet == FALSE)
 		{
@@ -113,7 +145,7 @@ void main()
 		while (1)
 		{
 			DWORD dwRet = WaitForSingleObject(hReadFile, 0);
-			
+
 			if (dwRet == WAIT_OBJECT_0)
 			{
 				printf("\n%s : %ld", "FinishWrite...", ov.InternalHigh);
@@ -134,10 +166,12 @@ void main()
 					dwSum += ov.InternalHigh;
 					printf("\n%s : %ld: %ld", "Write...", ov.InternalHigh, dwSum);
 				}
-			}	
+			}
 		}
 	}
+
 	printf("파일 복사 완료");
-	delete[] buf;
+	delete[] g_buf;
 	CloseHandle(hWriteFile);
 }
+*/
---
+++ 64_IO/iocp.cpp
@@ -0,0 +1,197 @@
+
+#include <windows.h>
+#include <iostream>
+#include <tchar.h>
+#include <string.h>
+#include <conio.h>
+
+#define MAX_READ_SIZE 4096
+#define MAX_WORK_THREAD 4
+
+class TIOCP
+{
+public:
+	HANDLE m_hIOCP;
+	HANDLE m_hFileRead;
+	HANDLE m_hFileWrite;
+
+	OVERLAPPED	m_hReadOV;
+	OVERLAPPED	m_hWriteOV;
+	HANDLE		m_hEventKillThread;
+
+	HANDLE m_hWorkThread[MAX_WORK_THREAD];
+	char m_szReadBuffer[MAX_READ_SIZE];
+public:
+	void Init();
+	void Frame();
+	static DWORD WINAPI WorkerThread(LPVOID param);
+	bool WaitForRead();
+	bool DispatchRead(DWORD dwTransfer);
+	bool DispatchWrite(DWORD dwTransfer);
+
+public:
+	~TIOCP() {}
+	void Release()
+	{
+		CloseHandle(m_hFileRead);
+		CloseHandle(m_hFileWrite);
+		CloseHandle(m_hWorkThread[0]);
+		CloseHandle(m_hWorkThread[1]);
+		CloseHandle(m_hWorkThread[2]);
+		CloseHandle(m_hWorkThread[3]);
+		CloseHandle(m_hIOCP);
+	}
+};
+
+bool TIOCP::DispatchRead(DWORD dwTransfer)
+{
+	DWORD dwWrite;
+	if (::WriteFile(m_hFileWrite, 
+		m_szReadBuffer, dwTransfer, &dwWrite, 
+		(LPOVERLAPPED)&m_hWriteOV) == FALSE)
+	{
+		//중요!!!:정상적인 경우. 비동기 입출력이 진행되고있음.
+		if (GetLastError() != ERROR_IO_PENDING)
+		{
+			return false;
+		}
+	}
+
+	//블럭단위로 읽어서 그 다음 블럭부터 읽으라고 알려주는 부분.(m_hReadOV.Offset을 사용)
+	LARGE_INTEGER data;
+	data.QuadPart = dwTransfer;
+
+	m_hReadOV.Offset += data.LowPart;
+	m_hReadOV.OffsetHigh += data.HighPart;
+
+	WaitForRead();
+	return true;
+}
+bool TIOCP::DispatchWrite(DWORD dwTransfer)
+{
+	//블럭단위로 써서 그 다음부터 쓰라는 부분.
+	LARGE_INTEGER data;
+	data.QuadPart = dwTransfer;
+
+	m_hWriteOV.Offset += data.LowPart;
+	m_hWriteOV.OffsetHigh += data.HighPart;
+	return true;
+}
+
+DWORD WINAPI TIOCP::WorkerThread(LPVOID param)
+{
+	TIOCP* iocp = (TIOCP*)param;
+
+	OVERLAPPED ov;
+	DWORD bytesTransfer;
+	ULONG_PTR keyValue;
+
+	while (1)
+	{
+		if (WAIT_OBJECT_0 == ::WaitForSingleObject(iocp->m_hEventKillThread, 1))
+		{
+			break;
+		}
+
+		//비동기 입출력의 작업결과를 확인하는 함수.
+		//::범위 지정함수. 
+		//::사용자 정의 함수와 헷갈리지 말라고 이함수는 여기 클래스가 아니라 상위에 있다고 알려주는 함수.
+		//IOCP QUEUE
+		bool bRet = ::GetQueuedCompletionStatus(iocp->m_hIOCP, 
+												&bytesTransfer, &keyValue, 
+												(LPOVERLAPPED*)&ov, 1);
+
+		if (bRet == TRUE)
+		{
+			if (keyValue == (ULONG_PTR)iocp->m_hFileRead)
+			{
+				iocp->DispatchRead(bytesTransfer);
+			}
+			else
+			{
+				iocp->DispatchWrite(bytesTransfer);
+			}
+		}
+		else
+		{
+			if (GetLastError() == WAIT_TIMEOUT)
+			{
+				continue;
+			}
+			//......
+		}
+	}
+	return 0;
+}
+void TIOCP::Init()
+{
+	m_hFileRead = CreateFile(L"../../data/data_1.zip",
+		GENERIC_READ, 0, 0, OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, 0);
+	m_hFileWrite = CreateFile(L"copy.zip",
+		GENERIC_WRITE, 0, 0, CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, 0);
+
+	//스레드4개를 고용.
+	DWORD id;
+	for (int i = 0; i < MAX_WORK_THREAD; i++)
+	{
+		m_hWorkThread[i] = ::CreateThread(0, 0, WorkerThread, this, 0, &id);
+	}
+
+	m_hEventKillThread = ::CreateEvent(0, TRUE, FALSE, NULL);
+	::ResetEvent(m_hEventKillThread);
+
+	//기술자를 고용.
+	m_hIOCP = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 0);
+
+	//기술자에게 Read파일에대한 읽는 결과가 완료되면, 그 결과를 완료키key( ex)1111 )의 내용으로 알려줘라.
+	::CreateIoCompletionPort(m_hFileRead, m_hIOCP, (ULONG_PTR)m_hFileRead, 0);
+	//기술자에게 Write파일에대한 쓰는 결과가 완료되면, 그 결과를 완료키key( ex)2222 )의 내용으로 알려줘라.
+	//완료키는 유일해야한다. 유일해야하므로 핸들을 넣는다.
+	::CreateIoCompletionPort(m_hFileWrite, m_hIOCP, (ULONG_PTR)m_hFileWrite, 0);
+}
+
+bool TIOCP::WaitForRead()
+{
+	DWORD dwReadBytes = 0;
+	if (::ReadFile(m_hFileRead, m_szReadBuffer, 
+		MAX_READ_SIZE, &dwReadBytes, &m_hReadOV) == FALSE)
+	{
+		//중요!!!:정상적인 경우. 비동기 입출력이 진행되고있음.
+		if (GetLastError() == ERROR_IO_PENDING)
+		{
+			return true;
+		}
+		return false;
+	}
+	//else
+	//{
+	//	//읽은 바이트수가 0바이트이거나 파일의 끝(ERROR_HANDLE_EOF)일 경우.
+	//	if (dwReadBytes == 0 || GetLastError() == ERROR_HANDLE_EOF)
+	//	{
+	//		return false;
+	//	}
+	//}
+	return true;
+}
+void TIOCP::Frame()
+{
+	WaitForRead();
+
+	//모든 스레드가 다끝날때까지 반환하지 않는다.
+	//스레드가 완료된걸 알려주는 함수. 대기함수.
+	while (WaitForMultipleObjects(
+			MAX_WORK_THREAD, 
+			m_hWorkThread,
+			TRUE, 1));
+	Release();
+}
+
+void main()
+{
+	TIOCP iocp;
+	iocp.Init();
+	iocp.Frame();
+	_getch();
+}
